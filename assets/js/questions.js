e3af9019a470fb0a3a6149efe25fd937613a367b
KTC96 <kyleclow96@gmail.com>
2023-10-19 16:33:17 +0000

    Fix error with merge

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,1 +1,12 @@
-const questions = [];
+// An array that holds all the questions and possible answers, including the correct answer.
+const questions = [
+  {
+    question: "What was used to make this sound?",
+    audio: "./assets/audio/test.mp3",
+    answers: ["Banana", "Spoons", "Custard", "Leaves", 2],
+  },
+  {
+    question: "What was used to make this sound?",
+    answers: ["Cloth", "Banana", "Car engine", "Human voice", 0],
+  },
+];
