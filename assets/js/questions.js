2c7a682dc8bef76cfc129a28c120d26ea17c70a7
Ulrike Riemenschneider <uriemenschneider@gmail.com>
2023-10-19 15:34:00 +0200

    Add simple questions.js file with sample questions

---
+++ assets/js/questions.js
@@ -0,0 +1,11 @@
+ // An array that holds all the questions and possible answers, including the corrent answer.
+ const allQuestionsEasy = [
+    {
+      question: 'What is used to make this sound?',
+      answers: ['Banana', 'Spoons', 'Custard', 'Leaves', 2],
+    },
+    {
+        question: 'What is used to make this sound?',
+        answers: ['Cloth', 'Banana', 'Car engine', 'Human voice', 0],
+      }
+  ];
