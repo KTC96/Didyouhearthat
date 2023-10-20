766e061951def07d8d18e9cd701887c40aa4e288
KTC96 <kyleclow96@gmail.com>
2023-10-20 23:27:35 +0000

    Add theme to game intro

--- assets/js/end.js
+++ assets/js/end.js
@@ -1,39 +1,47 @@
-// Variables stored in local Storage are retrieved 
+import { pauseTheme, playTheme } from "./script";
+// Variables stored in local Storage are retrieved
 // Elements are assigned variables so they can be filled with text content.
-let finalScore = document.getElementById('final-score');
-let finalScoreUser = document.getElementById('final-score-user');
+
+let finalScore = document.getElementById("final-score");
+let finalScoreUser = document.getElementById("final-score-user");
 let mostRecentScore = localStorage.getItem("mostRecentScore");
 let userLevel = localStorage.getItem("userLevel");
 let user = window.localStorage.getItem("username");
 
 // Set personalized content of text fields on final page
-finalScoreUser.innerText = `Hi ${user}! Your final score was:` ;
-finalScore.innerText = `${mostRecentScore} @ ${userLevel}` ;
+finalScoreUser.innerText = `Hi ${user}! Your final score was:`;
+finalScore.innerText = `${mostRecentScore} @ ${userLevel}`;
 
 // If statement designed to display a custom message depending on the users score
-if (userLevel == 'Movie') {
-    if (mostRecentScore < 4) {
-        document.getElementById('final-message').innerText = "Start Exploring!! There is so much more to learn!";
-    } else if (mostRecentScore <= 8) {
-        document.getElementById('final-message').innerText = "Nice work! See if you can improve!";
-    } else {
-        document.getElementById('final-message').innerText = "Excellent result! Why not try the next level?";
-    }
-} else if (userLevel == 'Sounds') {
-    if (mostRecentScore < 4) {
-        document.getElementById('final-message').innerText = "Good progress!! Keep learning!";
-    } else if (mostRecentScore <= 8) {
-        document.getElementById('final-message').innerText = "Take out a map. See if you can improve!";
-    } else {
-        document.getElementById('final-message').innerText = "Excellent result! Why not try the next level?";
-    }
-} 
+if (userLevel == "Movie") {
+  if (mostRecentScore < 4) {
+    document.getElementById("final-message").innerText =
+      "Start Exploring!! There is so much more to learn!";
+  } else if (mostRecentScore <= 8) {
+    document.getElementById("final-message").innerText =
+      "Nice work! See if you can improve!";
+  } else {
+    document.getElementById("final-message").innerText =
+      "Excellent result! Why not try the next level?";
+  }
+} else if (userLevel == "Sounds") {
+  if (mostRecentScore < 4) {
+    document.getElementById("final-message").innerText =
+      "Good progress!! Keep learning!";
+  } else if (mostRecentScore <= 8) {
+    document.getElementById("final-message").innerText =
+      "Take out a map. See if you can improve!";
+  } else {
+    document.getElementById("final-message").innerText =
+      "Excellent result! Why not try the next level?";
+  }
+}
 
 const playAgainSameUser = document.getElementById("play-again-same-user");
-playAgainSameUser.innerHTML = `<i class="fa-solid fa-rotate-left"></i> Play again as ${user}` ;
+playAgainSameUser.innerHTML = `<i class="fa-solid fa-rotate-left"></i> Play again as ${user}`;
 
 const playagain = document.getElementById("play-again-btn");
-playagain.addEventListener('click', returntostart);
+playagain.addEventListener("click", returntostart);
 
 /**
  * This function allows a user to clear the username stored and start the game over
@@ -41,22 +49,26 @@ playagain.addEventListener('click', returntostart);
  * @param {event} e - event of a click
  */
 function returntostart(e) {
-    e.preventDefault();
-    localStorage.clear();
-    load(startOver);
+  pauseTheme();
+  e.preventDefault();
+  localStorage.clear();
+  load(startOver);
+  playTheme();
 }
 
 /**
  * This function delays the loading of myURL
- * @param {string} myURL - URL is loaded with time delay 
+ * @param {string} myURL - URL is loaded with time delay
  */
 function load(myURL) {
-    setTimeout(myURL, 0);
+  setTimeout(myURL, 0);
 }
 
 /**
  * This function loads the last page when the game is over
  */
-function startOver () {
-    window.location.assign('index.html');
+function startOver() {
+  pauseTheme();
+  window.location.assign("index.html");
+  playTheme();
 }
